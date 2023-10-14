4953d6f81e9dfebaa0b3050ec4fe6e424f4e8f80
mshreeharsha <shreeharsha.mbhat@gmail.com>
2023-10-14 09:16:42 +0530

    Added Auth Context

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
-const authcontext = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+const AuthContext = createContext()
+
+const AuthContextProvider =({children})=>{
+  const [auth,setAuth] = useState({
+    user:null,
+    token:''
+  })
+
+  //Setting Default Header in Axios
+  axios.defaults.headers.common["Authorization"]=auth?.token
+  
+  useEffect(()=>{
+    //Fetching the Token from Local Storage
+
+    const data = localStorage.getItem('auth')
+
+    if(data){
+      const parseData=JSON.parse(data)
+      setAuth(
+        {
+          ...auth,
+          user:parseData.user,
+          token:parseData.token
+        }
+      )
+    }
+    //esline-disable-next-line
+  },[])
+
+  return(
+    <AuthContext.Provider value={[auth,setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  )
 }
 
-export default authcontext;
+//Creating a custom Hook
+
+const useAuthContext = ()=>{
+  return useContext(AuthContext)
+}
+
+export {useAuthContext,AuthContextProvider}
