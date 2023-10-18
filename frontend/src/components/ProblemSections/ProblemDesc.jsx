bb53424875c19961aa10c299a02832790cb03034
Vishwas1687 <97346738+Vishwas1687@users.noreply.github.com>
2023-10-18 16:36:59 +0530

    pushing-to-pull

--- frontend/src/components/ProblemSections/ProblemDesc.jsx
+++ frontend/src/components/ProblemSections/ProblemDesc.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios' 
+import { baseUrl } from '../../baseUrl';
 
 const ProblemDesc = () => {
+    const [problem,setProblem]=useState({})
+
+    const fetchProblem=async()=>{
+       try{
+        const {data}=await axios.get(`${baseUrl}/api/problems/single-problem`)
+        
+       }catch(error)
+       {
+         alert('Single problem is not fetching')
+       }
+    }
+   useEffect(()=>{
+     fetchProblem()
+   },[])
   return (
     <div>
        Description
